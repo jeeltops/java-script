ee5400f0f61b8e4faca4ca0228e7d9518882dbc7
jeel vithani <jeelvithanijeel@gmail.com>
2023-09-12 16:39:25 +0530

    first commit

---
+++ prectis/famaily_tree.js
@@ -0,0 +1,68 @@
+let familyTree = {
+   name: "Poptabhai",
+   age: 76,
+   child: [
+     {
+       id: 10001,
+       name: "Rajubhai",
+       age: 45,
+       occupation: "Diamond",
+       vehicle: [
+         {
+           name: "Hero",
+           number: 3060,
+         },
+       ],
+       son: [
+         {
+           name: "Mansi",
+           age: 20,
+         },
+         {
+           name: "Kano",
+           age: 17,
+         },
+       ],
+     },
+     {
+       id: 10002,
+       name: "Mukeshbhai",
+       age: 42,
+       occupation: "Diamond",
+       vehicle: [
+         {
+           name: "Ola",
+           number: 3060,
+         },
+       ],
+       son: [
+         {
+           name: "Jeel",
+           age: 20,
+         },
+         {
+           name: "Krisha",
+           age: 17,
+         },
+       ],
+     },
+     {
+      id: 10003,
+      name: "Montubhai",
+      age: 41,
+      occupation: "Diamond",
+      vehicle: [
+        {
+          name: "Activa",
+          number: 2356,
+        },
+      ],
+      son: [
+        {
+          name: "hari",
+          age: 8,
+        },
+      ],
+    },
+   ],
+ };
